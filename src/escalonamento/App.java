da0be466406be2575182c15cca3e612dfa80c7a8
Larissa Fiorini <larissa.fiorini@hotmail.com>
2018-10-04 11:51:48 -0300

    Update App.java

--- src/escalonamento/App.java
+++ src/escalonamento/App.java
@@ -9,9 +9,18 @@ import java.util.Scanner;
 
 
 /**
- * 
- * 
+ * Escalonamento Round Robin com prioridade
  *
+ * Este programa recebe um arquivo de entrada com as informações dos processos que desejam ser executados, realiza o escalonamento
+ * da execução desses processos utilizando o algoritmo Round Robin com prioridade. Ao final, mostra ao usuário um grafo ilustrando como 
+ * foram executados esses processos, bem como os tempos do algoritmo, sendo eles o tempo médio de espera e resposta.
+ * 
+ * Autores: 
+ * Larissa Fiorini Martins
+ * Renata Sirotsky Soria
+ *
+ * Disciplina: Sistemas Operacionais - PUCRS
+ * Data: 04.10.2018
  */
 
 
@@ -20,9 +29,8 @@ public class App {
 		System.out.println("****Algoritmo Round Robin****\n");
 		
 		Scanner scan = new Scanner(System.in);
-		// System.out.println("Insira o nome do arquivo: ");
-		// String file_name = scan.nextLine();
-		String file_name = "input.txt";
+		System.out.println("Insira o nome do arquivo: ");
+		String file_name = scan.nextLine();
 		scan.close();
 		
 		// Classe que controla lista de processos
